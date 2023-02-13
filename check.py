c21484ced712307738cf7f2250542129fa9a7697
KriuchkovSP <kryuchkovsp@mail.ru>
2023-02-13 16:22:49 +0500

    Add func nav menu, list notes, create, open, delete note

--- check.py
+++ check.py
@@ -1,12 +1,43 @@
-def input_data(nums_menu: int):
+import os
+import csv
+
+def input_data_menu(nums: int, text: str):
     flag_input_ok = False
     while not flag_input_ok:
         try:
-            num = int(input(f'Введите пункт меню от 1 до {nums_menu}: '))
-            if (num > 0 and num <= nums_menu):
+            num = int(input(f'{text}: '))
+            if (num > 0 and num <= nums):
                 flag_input_ok = True
                 return num
             else:
                 raise ValueError()
         except ValueError:
-            print("Введите целое число из указанного диапазона!")
+            print("Введено неверное число!")
+
+def input_data(text: str):
+    flag_input_ok = False
+    while not flag_input_ok:
+        try:
+            num = int(input(f'{text}: '))
+            flag_input_ok = True
+            return num
+        except ValueError:
+            print("Введено неверное число!")
+            
+def check_dir(directory: str):
+    if (not os.path.exists(directory)):
+        os.mkdir(directory)
+
+def check_file(filename: str):
+    if (not os.path.isfile(filename)):
+        with open(filename, 'a') as data:
+            csvwriter = csv.writer(data, delimiter = ";")
+            csvwriter.writerow(["id","Date","Header","Body"])
+            
+def check_note_id(data, id):
+    for i in range(1, len(data)):
+        if (id == int(data[i][0])):
+            return True
+    print("Заметки с таким id не существует")
+    print()
+    return False
--- notes/notes.py
+++ notes/notes.py
@@ -1,14 +1,69 @@
+import os
+from pyautogui import typewrite
+import format_lib.file_func, check
+
 def list():
     print('Список заметок')
-
+    data = format_lib.file_func.read_file('data/notes.csv')
+    print_all(data)
+    
 def create():
-    print('Создание заметки')
+    head = input("Введите заголовок новой заметки: ")
+    body = input("Введите содержимое заметки: ")
+    data = format_lib.file_func.read_file('data/notes.csv')
+    if (len(data)) == 1:
+        id = 1
+    else:
+        id = int(data[len(data) - 1][0]) + 1
+    format_lib.file_func.write_file('data/notes.csv',id,head,body)
+    read(id)
     
-def read():
-    print('Открытие заметки')
+def read(id):
+    data = format_lib.file_func.read_file('data/notes.csv')
+    if (check.check_note_id(data,id)):
+        print()
+        print_data(data,id)
+        return True
+    else:
+        return False
     
-def edit():
+def edit(id):
     print('Редактирование заметки')
+    data = format_lib.file_func.read_file('data/notes.csv')
+    if (check.check_note_id(data,id)):
+        print()
+        print_data(data,id)
+        return True
+    else:
+        return False
+        
+    print("enter folder name: ")
+    typewrite("Default Value")
+    folder = input()
     
-def delete():
-    print('Удаление заметки')
+def delete(id):
+    data = format_lib.file_func.read_file('data/notes.csv')
+    if (check.check_note_id(data,id)):
+        print()
+        elem = print_data(data,id)
+        del data[elem]
+        return (True, data)
+    else:
+        return (False, -1)
+    
+def delete_confirm(data):
+    format_lib.file_func.write_file('data/notes.csv',data)
+    
+def print_all(data):
+    for i in range(len(data)):
+        print(f'{data[i][0]:4s} {data[i][1]:18s} {data[i][2]}')
+
+def print_data(data, id):
+    for i in range(1,len(data)):
+        if (int(data[i][0]) == id):
+            print(f'ID заметки: {data[i][0]}')
+            print(f'Дата создания/изменения заметки: {data[i][1]}')
+            print(f'Заголовок заметки: {data[i][2]}')
+            print(f'Содержимое заметки: {data[i][3]}')
+            return i
+            
